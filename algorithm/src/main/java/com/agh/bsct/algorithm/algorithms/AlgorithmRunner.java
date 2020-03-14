89552f977e47be52b800407f62f08732675e2820
Artur <kawala.artek@gmail.com>
2020-03-14 17:50:19 +0100

    Refactor AlgorithmRunner

--- algorithm/src/main/java/com/agh/bsct/algorithm/algorithms/AlgorithmRunner.java
+++ algorithm/src/main/java/com/agh/bsct/algorithm/algorithms/AlgorithmRunner.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.agh.bsct.algorithm.algorithms.AAlgorithm.ANT_QUALIFIER;
 import static com.agh.bsct.algorithm.algorithms.BFAlgorithm.BRUTE_FORCE_QUALIFIER;
 import static com.agh.bsct.algorithm.algorithms.SAAlgorithm.SIMULATED_ANNEALING_QUALIFIER;
@@ -12,34 +15,30 @@ import static com.agh.bsct.algorithm.algorithms.SAAlgorithm.SIMULATED_ANNEALING_
 @Component
 public class AlgorithmRunner {
 
-    private static final String SA_ALGORITHM_SYMBOL = "sa";
-    private static final String A_ALGORITHM_SYMBOL = "a";
-    private static final String BF_ALGORITHM_SYMBOL = "bf";
+    private static final String SA_ALGORITHM_TYPE = "sa";
+    private static final String A_ALGORITHM_TYPE = "a";
+    private static final String BF_ALGORITHM_TYPE = "bf";
 
-    private final IAlgorithm saAlgorithm;
-    private final IAlgorithm aAlgorithm;
-    private final IAlgorithm bfAlgorithm;
+    private final Map<String, IAlgorithm> typeToImplementation = new HashMap<>();
 
     @Autowired
     public AlgorithmRunner(@Qualifier(SIMULATED_ANNEALING_QUALIFIER) IAlgorithm saAlgorithm,
                            @Qualifier(ANT_QUALIFIER) IAlgorithm aAlgorithm,
                            @Qualifier(BRUTE_FORCE_QUALIFIER) IAlgorithm bfAlgorithm) {
-        this.saAlgorithm = saAlgorithm;
-        this.aAlgorithm = aAlgorithm;
-        this.bfAlgorithm = bfAlgorithm;
+        typeToImplementation.put(SA_ALGORITHM_TYPE, saAlgorithm);
+        typeToImplementation.put(A_ALGORITHM_TYPE, aAlgorithm);
+        typeToImplementation.put(BF_ALGORITHM_TYPE, bfAlgorithm);
     }
 
     public void run(AlgorithmTask algorithmTask) {
         String algorithmType = algorithmTask.getAlgorithmType();
-        if (SA_ALGORITHM_SYMBOL.equals(algorithmType)) {
-            saAlgorithm.run(algorithmTask);
-        } else if (A_ALGORITHM_SYMBOL.equals(algorithmType)) {
-            aAlgorithm.run(algorithmTask);
-        } else if (BF_ALGORITHM_SYMBOL.equals(algorithmType)) {
-            bfAlgorithm.run(algorithmTask);
-        } else {
+        validateAlgorithmType(algorithmType);
+        typeToImplementation.get(algorithmType).run(algorithmTask);
+    }
+
+    private void validateAlgorithmType(String algorithmType) {
+        if (!typeToImplementation.containsKey(algorithmType)) {
             throw new IllegalStateException("Cannot find IAlgorithm implementation for type: " + algorithmType);
         }
     }
-
 }
