939c81309d288ab0cae3965db3398de5e03155a2
Artur <kawala.artek@gmail.com>
2020-06-15 22:09:40 +0200

    Update paths

--- data-collector/src/main/java/com/agh/bsct/datacollector/controllers/config/PathsConstants.java
+++ data-collector/src/main/java/com/agh/bsct/datacollector/controllers/config/PathsConstants.java
@@ -2,8 +2,8 @@ package com.agh.bsct.datacollector.controllers.config;
 
 public class PathsConstants {
 
-    public static final String ALGORITHM_ROOT_PATH = "http://localhost:8090/";
-    public static final String DATA_COLLECTOR_ROOT_PATH = "http://localhost:8080/";
+    public static final String ALGORITHM_ROOT_PATH = "https://cg-algorithm.herokuapp.com/";
+    public static final String DATA_COLLECTOR_ROOT_PATH = "https://cg-data-collector.herokuapp.com/";
 
     private PathsConstants() {
     }
